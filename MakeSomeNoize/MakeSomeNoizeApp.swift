25f307faaddbd7b1920aa229ad4aa2973c339de3
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-20 11:11:02 +0300

    Initial Commit

---
+++ MakeSomeNoize/MakeSomeNoizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeSomeNoizeApp.swift
+//  MakeSomeNoize
+//
+//  Created by Николай Игнатов on 20.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct MakeSomeNoizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
